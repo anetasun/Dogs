800c67c7403944c8bfcfec9260dcd339a4344360
anetasun <anetsn@gmail.com>
2024-11-18 16:36:55 +0300

    Добавлен новый виджет notyebook для отображения каждой картинки в новой вкладке

--- Dogs.py
+++ Dogs.py
@@ -30,8 +30,10 @@ def show_image():
             img = ImageTk.PhotoImage(img)
             new_window = Toplevel(window)
             new_window.title("Случайное изображение пёсика")
-            lb = ttk.Label(new_window, image=img)
-            lb.pack()
+            tab = ttk.Frame(notebook)
+            notebook.add(tab, text=f"Картинка{notebook.index("end")+1}")
+            lb = ttk.Label(tab, image=img)
+            lb.pack(padx=10, pady=10)
             lb.image = img
         except requests.RequestException as e:
            mb.showerror("Ошибка", f"Не удалось загрузить изображение: {e}")
@@ -70,6 +72,14 @@ height_label = ttk.Label(text="Высота:")
 height_label.pack(side='left', padx=(10, 0))
 height_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
 height_spinbox.pack(side='left', padx=(0, 10))
+
+# Создаем отдельное окно для Notebook
+top_level_window = Toplevel(window)
+top_level_window.title("Изображения пёсиков")
+
+notebook = ttk.Notebook(top_level_window)
+notebook.pack(expand=True, fill='both', padx=10, pady=10)
+
 
 
 window.mainloop()
